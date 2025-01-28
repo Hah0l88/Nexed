9a0d9e24f6f8af37da2b886e819a0f2a2ce2f125
Hah0l88 <ilya7002sv@gmail.com>
2025-01-28 10:06:33 +0100

    Update .gitignore

--- Python/x/les.py
+++
@@ -1,6 +0,0 @@
-naam = input("Wat is je naam?\n")
-achternaam = input("Wat is je achternaam?\n")
-lang = (input("Wat is jouwe lengte?\n"))
-leeftijd = input("Wat is jouwe leeftijt?\n")
-
-print("Jouwe naam is: " + naam  + " " + "jouwe achternaam is: " + achternaam +" " + "jouwe lengte: " + lang +"cm" + " " +"jouwe leeftijd: " + leeftijd)
